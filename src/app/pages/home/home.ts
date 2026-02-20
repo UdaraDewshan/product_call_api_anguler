43559136e0c1a538332510a6ddd67ec7bf06e110
UdaraDewshan <dewudara@gmail.com>
2026-02-20 11:44:31 +0530

    Add footer component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeBody } from "../../components/home-body/home-body";
 import { NavBar } from "../../components/nav-bar/nav-bar";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [HomeBody, NavBar],
+  imports: [HomeBody, NavBar, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
