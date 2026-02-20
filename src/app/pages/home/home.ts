508d668830e2cfa1936dd08392645e1adad3c78b
UdaraDewshan <dewudara@gmail.com>
2026-02-20 11:37:32 +0530

    Add Home Page

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HomeBody } from "../../components/home-body/home-body";
+
+@Component({
+  selector: 'app-home',
+  imports: [HomeBody],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
